1c27c698f344f4f9e21b644ea6481061f3fbc53a
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-08-24 01:08:44 +0600

    uuuu

--- ControlStatement.java
+++ ControlStatement.java
@@ -1,8 +1,24 @@
 package ControlStatement;
 
+import java.util.Scanner;
+
 public class ControlStatement {
 	public static void main(String[] args) {
 		//statement 
+		System.out.println("----STATEMENT----\n");
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter your number:\n");
+		
+		double number = input.nextDouble();
+		if(number > 0) {
+			System.out.println("This is a Positive number\n");
+		}
+		else if(number ==0 ) {
+			System.out.println("This is Zero\n");			
+		}
+		else {
+			System.out.println("This is a Nagetive number\n");
+		}
 		
 	}
 }
