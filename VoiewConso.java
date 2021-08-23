cb8034d96de0beb976b196ed22462b131c4990cd
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-08-24 01:25:03 +0600

    vowel conso

---
+++ VoiewConso.java
@@ -0,0 +1,34 @@
+package ControlStatement;
+
+import java.util.Scanner;
+
+public class VoiewConso {
+	public static void main(String[] args) {
+		System.out.println("---Voiew and Consonent---\n");
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter a character\n");
+		
+		char letter = input.next().charAt(0); // taking the first letter of the string
+		
+		
+		if(letter == 'a') {
+			System.out.println("This is voiew\n");
+		}
+		else if(letter == 'e') {
+			System.out.println("This is voiew\n");
+		}
+		else if(letter == 'i') {
+			System.out.println("This is voiew\n");
+		}
+		else if(letter == 'o') {
+			System.out.println("This is voiew\n");
+		}
+		else if(letter == 'u') {
+			System.out.println("This is voiew\n");
+		}
+		else {
+			System.out.println("This is consonent\n");
+		}
+	}
+}
